49c646f2dca29610909566b981b7f480146c2c7b
irchorn <70407458+irchorn@users.noreply.github.com>
2021-03-07 20:27:55 -0500

    Update IS211_Assignment5.py

--- IS211_Assignment5.py
+++ IS211_Assignment5.py
@@ -2,7 +2,17 @@
 # coding: utf-8
 
 # In[77]:
-
+class Queue:
+    def __init__(self):
+        self.items = [] 
+    def is_empty(self):
+        return self.items == [] 
+    def enqueue(self, item):
+        self.items.insert(0,item) 
+    def dequeue(self):
+        return self.items.pop() 
+    def size(self):
+        return len(self.items)
 
 class Server: 
     def __init__(self, ppm): 
